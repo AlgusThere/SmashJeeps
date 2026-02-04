b335994176a7ddb75a9ce6ccc7fbcc2ab8e41122
Algus <algukenan@gmail.com>
2026-02-05 01:06:22 +0300

    v0.2 Car Suspension

---
+++ Assets/_GameAssets/Scripts/SO/VehicleSettingsSO.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Vehicle Settings", menuName = "Scriptable Objects/ Vehicle Settings")]
+public class VehicleSettingsSO : ScriptableObject
+{
+    [Header("Wheel Paddings")]
+    [SerializeField] private float _wheelsPaddingX;
+    [SerializeField] private float _wheelsPaddingZ;
+
+    [Header("Suspension")]
+    [SerializeField] private float _springRestLenght;
+    [SerializeField] private float _springStrength;
+    [SerializeField] private float _springDamper;
+
+
+
+    public float WheelsPaddingX => _wheelsPaddingX;
+    public float WheelsPaddingZ => _wheelsPaddingZ;
+    public float SpringRestLength => _springRestLenght;
+    public float SpringStrength => _springStrength;
+    public float SpringDamper=> _springDamper;
+
+}
